1998ef23ca4401455e5354925c2801e4332132ae
yukinarit <yukinarit84@gmail.com>
2025-02-02 20:58:09 +0900

    Bootstrap tanu-integration-tests

---
+++ tanu-integration-tests/src/main.rs
@@ -0,0 +1,12 @@
+mod get;
+
+use tanu::eyre;
+
+#[tanu::main]
+#[tokio::main]
+async fn main() -> eyre::Result<()> {
+    let runner = run();
+    let app = tanu::App::new();
+    app.run(runner).await?;
+    Ok(())
+}
