aedc56529bddf4b0511bacb987052419df102bda
yukinarit <yukinarit84@gmail.com>
2025-06-11 23:06:16 +0900

    Refactor CLI to use clap builder API

--- tanu/src/app.rs
+++ tanu/src/app.rs
@@ -1,11 +1,90 @@
-use clap::Parser;
+use clap::{value_parser, Arg, ArgAction, Command as ClapCommand};
 use console::Term;
 use eyre::OptionExt;
 use itertools::Itertools;
-use std::{collections::HashMap, str::FromStr};
+use std::{
+    collections::{HashMap, VecDeque},
+    str::FromStr,
+};
 use tanu_core::Filter;
 
 use crate::{get_tanu_config, ListReporter, ReporterType, TableReporter};
+
+/// Build the CLI with clap's builder pattern
+fn build_cli<'a>(third_party_reporters: impl Iterator<Item = &'a String>) -> ClapCommand {
+    let mut reporter_choices: VecDeque<_> = third_party_reporters.map(|s| s.to_string()).collect();
+    reporter_choices.push_front(ReporterType::Table.to_string());
+    reporter_choices.push_front(ReporterType::List.to_string());
+    ClapCommand::new("tanu")
+        .about("tanu CLI offers various commands, including listing and executing test cases")
+        .version(env!("CARGO_PKG_VERSION"))
+        .subcommand_required(true)
+        .subcommand(
+            ClapCommand::new("test")
+                .about("Run tests in CLI mode")
+                .arg(Arg::new("capture-http")
+                    .long("capture-http")
+                    .help("Capture http debug logs")
+                    .action(ArgAction::SetTrue))
+                .arg(Arg::new("capture-rust")
+                    .long("capture-rust")
+                    .help("Capture Rust \"log\" crate based logs. This is usefull in the following two cases\n1) tanu failed unexpectedly and you would want to see the tanu's internal logs.\n2) you would want to see logs produced from your tests that uses \"log\" crate")
+                    .action(ArgAction::SetTrue))
+                .arg(Arg::new("projects")
+                    .short('p')
+                    .long("projects")
+                    .help("Specify projects to run in comma-separated string. --projects dev --projects staging")
+                    .value_delimiter(',')
+                    .action(ArgAction::Append))
+                .arg(Arg::new("modules")
+                    .short('m')
+                    .long("modules")
+                    .help("Specify modules to run in comma-separated string. --modules foo,bar")
+                    .value_delimiter(',')
+                    .action(ArgAction::Append))
+                .arg(Arg::new("tests")
+                    .short('t')
+                    .long("tests")
+                    .help("Specify test cases to run in comma-separated string. e.g. --tests a,b")
+                    .value_delimiter(',')
+                    .action(ArgAction::Append))
+                .arg(Arg::new("reporters")
+                    .long("reporters")
+                    .help(format!("Specify the reporters to use in comma-separated string. Default is \"list\". [possible values: {}]", reporter_choices.into_iter().join(", ")))
+                    .value_delimiter(',')
+                    .action(ArgAction::Append))
+                .arg(Arg::new("concurrency")
+                    .short('c')
+                    .long("concurrency")
+                    .help("Specify the maximum number of tests to run in parallel. When unspecified, all tests run in parallel")
+                    .value_parser(value_parser!(usize)))
+                .arg(Arg::new("color")
+                    .long("color")
+                    .help("Produce color output. Default is \"auto\" [env: CARGO_TERM_COLOR]")
+                    .value_parser(["auto", "always", "never"]))
+        )
+        .subcommand(
+            ClapCommand::new("tui")
+                .about("Run tests in TUI mode")
+                .arg(Arg::new("log-level")
+                    .long("log-level")
+                    .help("Log level filter")
+                    .default_value("Info"))
+                .arg(Arg::new("tanu-log-level")
+                    .long("tanu-log-level")
+                    .help("tanu log level filter")
+                    .default_value("Info"))
+                .arg(Arg::new("concurrency")
+                    .short('c')
+                    .long("concurrency")
+                    .help("Specify the maximum number of tests to run in parallel. Default is the number of logical CPU cores")
+                    .value_parser(value_parser!(usize)))
+        )
+        .subcommand(
+            ClapCommand::new("ls")
+                .about("List test cases")
+        )
+}
 
 /// tanu CLI.
 #[derive(Default)]
@@ -32,22 +111,36 @@ impl App {
 
     /// Parse command-line args and run tanu CLI sub command.
     pub async fn run(mut self, mut runner: crate::Runner) -> eyre::Result<()> {
-        let args = Args::parse();
+        let matches = build_cli(self.third_party_reporters.keys()).get_matches();
         color_eyre::install().unwrap();
 
         let cfg = get_tanu_config();
         let term = Term::stdout();
-        match args.command {
-            Command::Test {
-                capture_rust,
-                capture_http,
-                projects,
-                modules,
-                tests,
-                reporters: reporters_arg,
-                concurrency,
-                color: color_command,
-            } => {
+
+        match matches.subcommand() {
+            Some(("test", test_matches)) => {
+                let capture_http = test_matches.get_flag("capture-http");
+                let capture_rust = test_matches.get_flag("capture-rust");
+                let projects = test_matches
+                    .get_many::<String>("projects")
+                    .map(|vals| vals.cloned().collect::<Vec<_>>())
+                    .unwrap_or_default();
+                let modules = test_matches
+                    .get_many::<String>("modules")
+                    .map(|vals| vals.cloned().collect::<Vec<_>>())
+                    .unwrap_or_default();
+                let tests = test_matches
+                    .get_many::<String>("tests")
+                    .map(|vals| vals.cloned().collect::<Vec<_>>())
+                    .unwrap_or_default();
+                let reporters_arg = test_matches
+                    .get_many::<String>("reporters")
+                    .map(|vals| vals.cloned().collect::<Vec<_>>());
+                let concurrency = test_matches.get_one::<usize>("concurrency").cloned();
+                let color_command = test_matches
+                    .get_one::<String>("color")
+                    .and_then(|s| Color::from_str(s).ok());
+
                 if capture_http {
                     runner.capture_http();
                 }
@@ -104,11 +197,15 @@ impl App {
 
                 runner.run(&projects, &modules, &tests).await
             }
-            Command::Tui {
-                log_level,
-                tanu_log_level,
-                concurrency,
-            } => {
+            Some(("tui", tui_matches)) => {
+                let log_level_str = tui_matches.get_one::<String>("log-level").unwrap();
+                let tanu_log_level_str = tui_matches.get_one::<String>("tanu-log-level").unwrap();
+                let log_level =
+                    log::LevelFilter::from_str(log_level_str).unwrap_or(log::LevelFilter::Info);
+                let tanu_log_level = log::LevelFilter::from_str(tanu_log_level_str)
+                    .unwrap_or(log::LevelFilter::Info);
+                let concurrency = tui_matches.get_one::<usize>("concurrency").cloned();
+
                 if let Some(concurrency) = concurrency {
                     runner.set_concurrency(concurrency);
                 } else {
@@ -117,7 +214,7 @@ impl App {
 
                 tanu_tui::run(runner, log_level, tanu_log_level).await
             }
-            Command::Ls {} => {
+            Some(("ls", _)) => {
                 let filter = tanu_core::runner::TestIgnoreFilter::default();
                 let list = runner.list();
                 let test_case_by_module = list.iter().into_group_map_by(|test| test.module.clone());
@@ -142,16 +239,9 @@ impl App {
 
                 Ok(())
             }
+            _ => unreachable!("Subcommand required is set to true"),
         }
     }
-}
-
-/// tanu CLI offers various commands, including listing and executing test cases.
-#[derive(clap::Parser, Debug)]
-#[command(version, about, long_about = None)]
-pub struct Args {
-    #[command(subcommand)]
-    command: Command,
 }
 
 #[derive(Debug, Clone, Default, strum::EnumString)]
@@ -161,52 +251,4 @@ pub enum Color {
     Auto,
     Always,
     Never,
-}
-
-#[derive(clap::Subcommand, Debug)]
-pub enum Command {
-    /// Run tests in CLI mode
-    Test {
-        /// Capture http debug logs.
-        #[arg(long)]
-        capture_http: bool,
-        /// Capture Rust "log" crate based logs. This is usefull in the following two cases
-        /// 1) tanu failed unexpectedly and you would want to see the tanu's internal logs.
-        /// 2) you would want to see logs produced from your tests that uses "log" crate.
-        #[arg(long)]
-        capture_rust: bool,
-        /// Specify projects to run in comma-separated string.
-        /// --projects dev --projects staging
-        #[arg(short, long, value_delimiter = ',')]
-        projects: Vec<String>,
-        /// Specify modules to run in comma-separated string.
-        /// --modules foo,bar
-        #[arg(short, long, value_delimiter = ',')]
-        modules: Vec<String>,
-        /// Specify test cases to run in comma-separated string.
-        /// e.g. --tests a,b
-        #[arg(short, long, value_delimiter = ',')]
-        tests: Vec<String>,
-        /// Specify the reporters to use in comma-separated string. Default is "list". Possible values are "table", "list" and "null".
-        #[arg(long, value_delimiter = ',')]
-        reporters: Option<Vec<String>>,
-        /// Specify the maximum number of tests to run in parallel. When unspecified, all tests run in parallel.
-        #[arg(short, long)]
-        concurrency: Option<usize>,
-        /// Produce color output: "auto", "always" and "never". Default is "auto" [env: CARGO_TERM_COLOR]
-        #[arg(long)]
-        color: Option<Color>,
-    },
-    /// Run tests in TUI mode
-    Tui {
-        #[arg(long, default_value = "Info")]
-        log_level: log::LevelFilter,
-        #[arg(long, default_value = "Info")]
-        tanu_log_level: log::LevelFilter,
-        /// Specify the maximum number of tests to run in parallel. Default is the number of logical CPU cores.
-        #[arg(short, long)]
-        concurrency: Option<usize>,
-    },
-    /// List test cases
-    Ls {},
 }
